f56171bc9113ddf9412ee11cebcef315d81006e5
Matheus Mendes <msmmendes211@gmail.com>
2024-04-12 03:45:34 -0300

    Adiciona o EventController

---
+++ src/main/java/br/com/mendes/passin/controllers/EventController.java
@@ -0,0 +1,32 @@
+package br.com.mendes.passin.controllers;
+
+import br.com.mendes.passin.dto.event.EventIdDTO;
+import br.com.mendes.passin.dto.event.EventRequestDTO;
+import br.com.mendes.passin.dto.event.EventResponseDTO;
+import br.com.mendes.passin.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/events")
+public class EventController {
+    private final EventService eventService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
+        EventResponseDTO event = this.eventService.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+}
