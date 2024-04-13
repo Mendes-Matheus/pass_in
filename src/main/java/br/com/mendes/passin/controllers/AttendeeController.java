4bc4a92286c088ae224cde8400773498a454dac4
Matheus Mendes <msmmendes211@gmail.com>
2024-04-13 00:35:43 -0300

    feat: adiciona, à camada de controle, a classe AttendeeController e o método getAttendeeBadge, que permite visualizar o crachá de inscrição de cada participante do evento

---
+++ src/main/java/br/com/mendes/passin/controllers/AttendeeController.java
@@ -0,0 +1,24 @@
+package br.com.mendes.passin.controllers;
+
+import br.com.mendes.passin.dto.attendee.AttendeeBadgeResponseDTO;
+import br.com.mendes.passin.services.AttendeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/attendees")
+public class AttendeeController {
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        AttendeeBadgeResponseDTO response = this.attendeeService.getAttendBadge(attendeeId, uriComponentsBuilder);
+        return ResponseEntity.ok(response);
+    }
+}
